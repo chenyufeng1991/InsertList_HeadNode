e5990f80943e121ffe42eafeffdc5bcea5ffd947
chenyufeng <503434355@qq.com>
2016-03-01 17:19:41 +0800

    commit

--- InsertList_HeadNode/main.c
+++ InsertList_HeadNode/main.c
@@ -6,10 +6,131 @@
 //  Copyright © 2016年 chenyufengweb. All rights reserved.
 //
 
+/**
+ *  在pos位置插入值为x的一个节点
+ */
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int elemType;
+typedef struct NodeList{
+
+    int element;
+    struct NodeList *next;
+}Node;
+
+//初始化带头结点的单链表
+void InitialList(Node **pNode){
+
+    //个人建议每一次malloc分配内存空间后，都要进行判断分配是否成功，也即判断是否为空；
+    //此时的这个pNode就是一个头结点；
+    //初始化成功后，其实相当于是一个正常的链表了；
+    *pNode = (Node *)malloc(sizeof(Node));
+    if (*pNode == NULL) {
+        printf("%s函数执行，内存分配失败，初始化单链表失败",__FUNCTION__);
+    }else{
+
+        (*pNode)->next = NULL;
+        printf("%s函数执行，带头结点的单链表初始化完成\n",__FUNCTION__);
+    }
+}
+
+//创建带头结点的单链表（尾插法）
+void CreateListTailInsert(Node *pNode){
+
+    /**
+     *  就算一开始输入的数字小于等于0，带头结点的单链表都是会创建成功的，只是这个单链表为空而已，也就是里面除了头结点就没有其他节点了。
+     */
+    Node *pInsert;
+    Node *pMove;
+    pInsert = (Node *)malloc(sizeof(Node));//需要检测分配内存是否成功 pInsert == NULL  ?
+    memset(pInsert, 0, sizeof(Node));
+    pInsert->next = NULL;
+
+    scanf("%d",&(pInsert->element));
+    pMove = pNode;
+    while (pInsert->element > 0) {
+
+        pMove->next = pInsert;
+        pMove = pInsert;//pMove始终指向最后一个节点
+
+        pInsert = (Node *)malloc(sizeof(Node)); //需要检测分配内存是否成功 pInsert == NULL  ?
+        memset(pInsert, 0, sizeof(Node));
+        pInsert->next = NULL;
+
+        scanf("%d",&(pInsert->element));
+    }
+
+    printf("%s函数执行,带头结点的单链表使用尾插法创建成功\n",__FUNCTION__);
+}
+
+//打印带头结点的单链表
+void PrintList(Node *pNode){
+    /**
+     *  注意这里，如果单链表为空（只有一个头结点），我也让它打印（打印成功）。只是里面没有元素，打出来是空的而已,所以在控制台上就是一行空的；
+     */
+    Node *pMove;
+    pMove = pNode->next;
+    while (pMove != NULL) {
+        printf("%d ",pMove->element);
+        pMove = pMove->next;
+    }
+
+    printf("\n%s函数执行，打印带头结点的单链表成功\n",__FUNCTION__);
+}
+
+Node *InsertNode(Node *pNode,int pos,int x){
+
+    int i = 0;
+    Node *pMove;
+    Node *pInsert;
+    pInsert = (Node*)malloc(sizeof(Node));
+    memset(pInsert, 0, sizeof(Node));
+    pInsert->next = NULL;
+    pInsert->element = x;
+
+    pMove = pNode;
+    while (pMove != NULL) {
+        if (i == pos) {
+            pInsert->next = pMove->next;
+            pMove->next = pInsert;
+            printf("%s函数执行，在pos=%d位置插入value=%d成功\n",__FUNCTION__,pos,x);
+            return pNode;
+        }
+        i++;
+        pMove = pMove->next;
+    }
+
+    printf("%s函数执行，插入节点失败\n",__FUNCTION__);
+
+    return pNode;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    Node *pList;
+
+    InitialList(&pList);
+
+    CreateListTailInsert(pList);
+    PrintList(pList);
+
+    InsertNode(pList, 0, 999);
+    PrintList(pList);
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
